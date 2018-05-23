82e706e928fa00caa02e89d57f4b42b73ecf47c8
Jantz <jantzillac.93@gmail.com>
2018-05-23 16:59:39 -0500

    Change RecipeListAdapter to extend ArrayAdapter instead of BaseAdapter and accept a Recipe object ArrayList

--- app/src/main/java/com/creativesourceapps/android/cupboard/RecipeListAdapter.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/RecipeListAdapter.java
@@ -3,51 +3,30 @@ package com.creativesourceapps.android.cupboard;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class RecipeListAdapter extends BaseAdapter {
-    // Keeps track of the context and list of recipes to display
-    private Context mContext;
-    private List<String> mRecipes;
+public class RecipeListAdapter extends ArrayAdapter<Recipe> {
 
     /**
      * Constructor method
      * @param recipes The list of recipes to display
      */
-    public RecipeListAdapter(Context context, List<String> recipes) {
-        mContext = context;
-        mRecipes = recipes;
-    }
-
-    /**
-     * Returns the number of items the adapter will display
-     */
-    @Override
-    public int getCount() {
-        return mRecipes.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
+    public RecipeListAdapter(Context context, ArrayList<Recipe> recipes) {
+        super(context,0,recipes);
     }
 
     /**
      * Creates a new TextView for each item referenced by the adapter
      */
     public View getView(final int position, View convertView, ViewGroup parent) {
+        Recipe recipes = getItem(position);
         TextView textView;
         if (convertView == null) {
             // If the view is not recycled, this creates a new TextView to hold a string
-            textView = new TextView(mContext);
+            textView = new TextView(getContext());
             // Define the layout parameters
             textView.setPadding(8, 8, 8, 8);
         } else {
@@ -55,7 +34,7 @@ public class RecipeListAdapter extends BaseAdapter {
         }
 
         // Set the text resource and return the newly created TextView
-        textView.setText(mRecipes.get(position));
+        textView.setText(recipes.title);
         return textView;
     }
 
