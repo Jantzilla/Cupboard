29652b1ee6c0ce1f9e50011b26031f2c66761f19
Jantz <jantzillac.93@gmail.com>
2018-05-23 23:26:26 -0500

    Replace network request with OkHttp library implementation

--- app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
@@ -14,8 +14,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestRecipeData() {
-        String response;
 
         //Default Recipe API endpoint
         String myUrl = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
@@ -36,53 +41,71 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<Recipe> recipes = new ArrayList<>();
         recipes.clear();
 
-            HttpRequest getRequest = new HttpRequest();
 
-            if (isOnline()) {
-                try {
-                    response = getRequest.execute(myUrl).get();
+         if (isOnline()) {
 
-                    JSONArray json = new JSONArray(response);
+                OkHttpClient client = new OkHttpClient();
 
-                    for (int i = 0; i < json.length(); i++) {
-                        JSONObject resultObject = json.getJSONObject(i);
-                        Recipe recipe = new Recipe(resultObject.getInt("id"),
-                                resultObject.getString("name"));
-                        recipes.add(recipe);
+                Request request = new Request.Builder()
+                        .url(myUrl)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
                     }
 
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        if (!response.isSuccessful()) {
+                            throw new IOException("Unexpected code " + response);
+                        }
+                        try {
+
+                            JSONArray json = new JSONArray(response.body().string());
+
+                            for (int i = 0; i < json.length(); i++) {
+                                JSONObject resultObject = json.getJSONObject(i);
+                                Recipe recipe = new Recipe(resultObject.getInt("id"),
+                                        resultObject.getString("name"));
+                                recipes.add(recipe);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                        }
+
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //Handle UI here
+                                RecipeListAdapter moviesAdapter = new RecipeListAdapter(getApplicationContext(), recipes);
+
+                                GridView gridView = findViewById(R.id.recipes_grid_view);
+                                gridView.setAdapter(moviesAdapter);
+
+                                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view,
+                                                            int position, long id) {
+
+                                        Recipe item_clicked = recipes.get(position);
+
+                                    }
+                                });
+
+                            }
+                        });
+                    }
+                });
+
             } else {
                 Toast.makeText(this, "No Network Connection. Please reconnect and reload page.", Toast.LENGTH_LONG).show();
             }
-
-
-
-        RecipeListAdapter moviesAdapter = new RecipeListAdapter(this, recipes);
-
-        GridView gridView = findViewById(R.id.recipes_grid_view);
-        gridView.setAdapter(moviesAdapter);
-
-            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-
-                    Recipe item_clicked = recipes.get(position);
-
-
-
-                }
-            });
 
     }
 
