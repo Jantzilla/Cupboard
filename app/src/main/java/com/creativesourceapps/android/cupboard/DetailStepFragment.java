690df3803592d400eade4ebc892fab5619f04e53
Jantz <jantzillac.93@gmail.com>
2018-05-27 17:40:45 -0500

    Implement ExoPlayer inside of DetailStepFragment and initialize with step detail media url when available, otherwise use setVisibility(View.GONE)

--- app/src/main/java/com/creativesourceapps/android/cupboard/DetailStepFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/DetailStepFragment.java
@@ -1,19 +1,45 @@
 package com.creativesourceapps.android.cupboard;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 
-public class DetailStepFragment extends Fragment {
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+
+public class DetailStepFragment extends Fragment implements ExoPlayer.EventListener {
     int position;
+    private static final String TAG = DetailStepFragment.class.getSimpleName();
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+    private static MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
 
     // Override onAttach to make sure that the container activity has implemented the callback
     @Override
@@ -29,8 +55,8 @@ public class DetailStepFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         final View rootView = inflater.inflate(R.layout.fragment_detail_step, container, false);
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
         final TextView textView = (TextView) rootView.findViewById(R.id.text_description);
         Button previousBtn = (Button) rootView.findViewById(R.id.buttonA);
         Button nextBtn = (Button) rootView.findViewById(R.id.buttonB);
@@ -38,15 +64,57 @@ public class DetailStepFragment extends Fragment {
         position = getActivity().getIntent().getIntExtra("position", 0);
         textView.setText(recipe.instructions.get(position));
 
+        mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource
+                (getResources(), R.drawable.ic_launcher_foreground));
+
+        initializeMediaSession();
+
+        if(!recipe.media.get(position).equals(""))
+            initializePlayer(Uri.parse(recipe.media.get(position)));
+        else
+            mPlayerView.setVisibility(View.GONE);
+
          previousBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
                  if(position != 0) {
                      position -= 1;
                      textView.setText(recipe.instructions.get(position));
+
+                     if(mExoPlayer == null) {
+                         if (!recipe.media.get(position).equals("")) {
+                             mPlayerView.setVisibility(View.VISIBLE);
+                             initializePlayer(Uri.parse(recipe.media.get(position)));
+                         } else
+                             mPlayerView.setVisibility(View.GONE);
+                     } else {
+                         if (!recipe.media.get(position).equals("")) {
+                             releasePlayer();
+                             mPlayerView.setVisibility(View.VISIBLE);
+                             initializePlayer(Uri.parse(recipe.media.get(position)));
+                         } else {
+                             mPlayerView.setVisibility(View.GONE);
+                             releasePlayer();
+                         }
+                     }
                  } else {
                      position = recipe.instructions.size() - 1;
                      textView.setText(recipe.instructions.get(recipe.instructions.size() - 1));
+
+                     if(mExoPlayer == null) {
+                         if (!recipe.media.get(position).equals(""))
+                             initializePlayer(Uri.parse(recipe.media.get(position)));
+                         else
+                             mPlayerView.setVisibility(View.GONE);
+                     } else {
+                         if (!recipe.media.get(position).equals("")) {
+                             releasePlayer();
+                             initializePlayer(Uri.parse(recipe.media.get(position)));
+                         } else {
+                             mPlayerView.setVisibility(View.GONE);
+                             releasePlayer();
+                         }
+                     }
                  }
              }
          });
@@ -57,14 +125,193 @@ public class DetailStepFragment extends Fragment {
                 if(position != recipe.instructions.size() - 1) {
                     position += 1;
                     textView.setText(recipe.instructions.get(position));
+
+                    if(mExoPlayer == null) {
+                        if (!recipe.media.get(position).equals("")) {
+                            mPlayerView.setVisibility(View.VISIBLE);
+                            initializePlayer(Uri.parse(recipe.media.get(position)));
+                        } else
+                            mPlayerView.setVisibility(View.GONE);
+                    } else {
+                        if (!recipe.media.get(position).equals("")) {
+                            releasePlayer();
+                            mPlayerView.setVisibility(View.VISIBLE);
+                            initializePlayer(Uri.parse(recipe.media.get(position)));
+                        } else {
+                            mPlayerView.setVisibility(View.GONE);
+                            releasePlayer();
+                        }
+                    }
                 } else {
                     position = 0;
                     textView.setText(recipe.instructions.get(0));
+
+                    if(mExoPlayer == null) {
+                        if (!recipe.media.get(position).equals(""))
+                            initializePlayer(Uri.parse(recipe.media.get(position)));
+                        else
+                            mPlayerView.setVisibility(View.GONE);
+                    } else {
+                        if (!recipe.media.get(position).equals("")) {
+                            releasePlayer();
+                            initializePlayer(Uri.parse(recipe.media.get(position)));
+                        } else {
+                            mPlayerView.setVisibility(View.GONE);
+                            releasePlayer();
+                        }
+                    }
                 }
             }
         });
 
         // Return the root view
         return rootView;
+    }
+
+    private void initializeMediaSession() {
+
+        // Create a MediaSessionCompat.
+        mMediaSession = new MediaSessionCompat(getContext(), TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls.
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible.
+        mMediaSession.setMediaButtonReceiver(null);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player.
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+
+        // MySessionCallback has methods that handle callbacks from a media controller.
+        mMediaSession.setCallback(new MySessionCallback());
+
+        // Start the Media Session since the activity is active.
+        mMediaSession.setActive(true);
+
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            // Set the ExoPlayer.EventListener to this activity.
+            mExoPlayer.addListener(this);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "Cupboard");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mExoPlayer != null)
+            releasePlayer();
+        mMediaSession.setActive(false);
+    }
+
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == ExoPlayer.STATE_READY) && playWhenReady){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        } else if((playbackState == ExoPlayer.STATE_READY)){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+    }
+
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
+    }
+
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
+        }
     }
 }
