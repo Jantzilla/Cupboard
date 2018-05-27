1af3c5c654c7c41199e28148eae474d9641afd82
Jantz <jantzillac.93@gmail.com>
2018-05-27 00:53:55 -0500

    Add previous and next step button functionality

--- app/src/main/java/com/creativesourceapps/android/cupboard/DetailStepFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/DetailStepFragment.java
@@ -8,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class DetailStepFragment extends Fragment {
+    int position;
 
     // Override onAttach to make sure that the container activity has implemented the callback
     @Override
@@ -29,10 +31,38 @@ public class DetailStepFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_detail_step, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.text_description);
+        final TextView textView = (TextView) rootView.findViewById(R.id.text_description);
+        Button previousBtn = (Button) rootView.findViewById(R.id.buttonA);
+        Button nextBtn = (Button) rootView.findViewById(R.id.buttonB);
         final Recipe recipe = getActivity().getIntent().getParcelableExtra("parcel_data");
-        final int position = getActivity().getIntent().getIntExtra("position", 0);
+        position = getActivity().getIntent().getIntExtra("position", 0);
         textView.setText(recipe.instructions.get(position));
+
+         previousBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 if(position != 0) {
+                     position -= 1;
+                     textView.setText(recipe.instructions.get(position));
+                 } else {
+                     position = recipe.instructions.size() - 1;
+                     textView.setText(recipe.instructions.get(recipe.instructions.size() - 1));
+                 }
+             }
+         });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(position != recipe.instructions.size() - 1) {
+                    position += 1;
+                    textView.setText(recipe.instructions.get(position));
+                } else {
+                    position = 0;
+                    textView.setText(recipe.instructions.get(0));
+                }
+            }
+        });
 
         // Return the root view
         return rootView;
