88f916beff21431f49f439c1cac50c8e945bb64d
Jantz <jantzillac.93@gmail.com>
2018-05-28 22:07:25 -0500

    Create MainActivityInstrumentationTest to test Recipe GridView

---
+++ app/src/androidTest/java/com/creativesourceapps/android/cupboard/MainActivityInstrumentationTest.java
@@ -0,0 +1,29 @@
+package com.creativesourceapps.android.cupboard;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.anything;
+
+// Tests for MainActivity
+public class MainActivityInstrumentationTest {
+
+    // Preferred JUnit 4 mechanism of specifying the activity to be launched before each test
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickOnItemAtPositionTwo() {
+        // Directly specify the position in the adapter to click on
+        onData(anything()) // We are using the position so don't need to specify a data matcher
+                .inAdapterView(withId(R.id.recipes_grid_view)) // Specify the explicit id of the GridView
+                .atPosition(1) // Explicitly specify the adapter item to use
+                .perform(click()); // Standard ViewAction
+    }
+}
