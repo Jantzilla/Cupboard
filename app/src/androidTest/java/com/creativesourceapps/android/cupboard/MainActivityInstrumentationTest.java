096a8c1b7c5a38f66d6611e8a3fa15301b3a6d6c
Jantz <jantzillac.93@gmail.com>
2018-06-01 01:24:53 -0500

    Add clickOnItemAtPositionThree UI test

--- app/src/androidTest/java/com/creativesourceapps/android/cupboard/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/creativesourceapps/android/cupboard/MainActivityInstrumentationTest.java
@@ -20,10 +20,27 @@ public class MainActivityInstrumentationTest {
 
     @Test
     public void clickOnItemAtPositionTwo() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Directly specify the position in the adapter to click on
         onData(anything()) // We are using the position so don't need to specify a data matcher
                 .inAdapterView(withId(R.id.recipes_grid_view)) // Specify the explicit id of the GridView
                 .atPosition(1) // Explicitly specify the adapter item to use
+                .perform(click()); // Standard ViewAction
+    }
+
+    @Test
+    public void clickOnItemAtPositionThree() {
+
+        // Directly specify the position in the adapter to click on
+        onData(anything()) // We are using the position so don't need to specify a data matcher
+                .inAdapterView(withId(R.id.recipes_grid_view)) // Specify the explicit id of the GridView
+                .atPosition(2) // Explicitly specify the adapter item to use
                 .perform(click()); // Standard ViewAction
     }
 }
