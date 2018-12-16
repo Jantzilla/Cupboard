2be9087844f4742f5e25117a6c59ad9f1fd78052
Jantz <jantzillac.93@gmail.com>
2018-12-16 04:09:09 -0600

    Create new StepPagerFragment for view paging
    Because each recipe step will now be in it's own
    (DetailStepFragment) fragment, this new fragment will be used to
    feature the individual recipe step fragments.

---
+++ app/src/main/java/com/creativesourceapps/android/cupboard/StepPagerFragment.java
@@ -0,0 +1,24 @@
+package com.creativesourceapps.android.cupboard;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StepPagerFragment extends Fragment {
+
+    public StepPagerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_step_pager, container, false);
+
+
+        return view;
+    }
+}
