cf2501309af2da75203d64064d05412a8ee19a00
Jantz <jantzillac.93@gmail.com>
2018-12-16 11:27:41 -0600

    Add menu button to custom Toolbar & open drawer on click

--- app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -28,6 +33,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     private boolean isOnline() {
         ConnectivityManager cm =
@@ -157,11 +163,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         requestRecipeData();
     }
 }
