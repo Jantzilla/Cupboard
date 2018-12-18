37f3c115b8ac1c683815936a1b57846564caca95
Jantz <jantzillac.93@gmail.com>
2018-12-18 05:57:21 -0600

    Complete cupboard fragment adapter implementation

--- app/src/main/java/com/creativesourceapps/android/cupboard/CupboardAdapter.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/CupboardAdapter.java
@@ -1,37 +1,54 @@
 package com.creativesourceapps.android.cupboard;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CupboardAdapter extends RecyclerView.Adapter<CupboardAdapter.CupboardViewHolder> {
+    private ArrayList<String> categoryList = new ArrayList<>();
+    private Context context;
+    private LayoutInflater inflater;
 
-    public void CupboardAdapter() {
-
+    public void CupboardAdapter(ArrayList<String> categoryList) {
+        this.categoryList.addAll(categoryList);
     }
 
     @NonNull
     @Override
     public CupboardViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        inflater = LayoutInflater.from(context);
 
-        return null;
+        View view = inflater.inflate(R.layout.list_item_cupboard, viewGroup, false);
+
+        CupboardViewHolder viewHolder = new CupboardViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CupboardViewHolder cupboardViewHolder, int i) {
-
+        cupboardViewHolder.categoryTextView.setText(categoryList.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryList.size();
     }
 
     public class CupboardViewHolder extends RecyclerView.ViewHolder {
+        TextView categoryTextView;
 
         public CupboardViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            categoryTextView = itemView.findViewById(R.id.tv_category);
         }
 
     }
