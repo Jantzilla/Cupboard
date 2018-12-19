177a0cf7b8f45aa4fe5274389f778356dd26b707
Jantz <jantzillac.93@gmail.com>
2018-12-19 04:40:58 -0600

    Set transition xml as shared enter transition in cupboard detail

--- app/src/main/java/com/creativesourceapps/android/cupboard/DetailCupboardFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/DetailCupboardFragment.java
@@ -2,6 +2,8 @@ package com.creativesourceapps.android.cupboard;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,14 @@ public class DetailCupboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            Transition transition = TransitionInflater.from(getContext())
+                    .inflateTransition(R.transition.cupboard_shared_element_transition);
+
+            setSharedElementEnterTransition(transition);
+        }
+
         return inflater.inflate(R.layout.fragment_detail_cupboard, container, false);
     }
 
