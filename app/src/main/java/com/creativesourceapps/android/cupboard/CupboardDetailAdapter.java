912fa091db91541549ae6e548587c3e2a9a9fb84
Jantz <jantzillac.93@gmail.com>
2018-12-20 11:14:45 -0600

    Edit cupboard detail fragment RecyclerView adapter
    Changes include adding view references, defining members
    & adding constructor parameters.

--- app/src/main/java/com/creativesourceapps/android/cupboard/CupboardDetailAdapter.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/CupboardDetailAdapter.java
@@ -1,36 +1,55 @@
 package com.creativesourceapps.android.cupboard;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CupboardDetailAdapter extends RecyclerView.Adapter<CupboardDetailAdapter.DetailViewHolder> {
+    private ArrayList<Ingredient> ingredientsList = new ArrayList<>();
 
-    public CupboardDetailAdapter() {
+    private Context context;
+    private LayoutInflater layoutInflator;
 
+    public CupboardDetailAdapter(ArrayList<Ingredient> ingredientsList) {
+        this.ingredientsList.addAll(ingredientsList);
     }
 
     @NonNull
     @Override
     public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        context = viewGroup.getContext();
+        layoutInflator = LayoutInflater.from(context);
+        View view = layoutInflator.inflate(R.layout.list_item_cupboard_detail, viewGroup, false);
+        DetailViewHolder detailViewHolder = new DetailViewHolder(view);
+
+        return detailViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int i) {
-
+        detailViewHolder.ingredientTextView = ingredientsList.get(i).name;
+        detailViewHolder.quantityTextView = ingredientsList.get(i).quantity;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return ingredientsList.size();
     }
 
     class DetailViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView ingredientTextView, quantityTextView;
 
         public DetailViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            ingredientTextView = itemView.findViewById(R.id.tv_ingredient);
+            quantityTextView = itemView.findViewById(R.id.tv_quantity);
 
             itemView.setOnClickListener(this);
         }
