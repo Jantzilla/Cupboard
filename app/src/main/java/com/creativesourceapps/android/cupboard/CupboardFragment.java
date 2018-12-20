287d8830ff1b8e09876ff2933f5a25c14108d64e
Jantz <jantzillac.93@gmail.com>
2018-12-20 16:29:28 -0600

    Add transition to cupboard detail fragment on cupboard FAB click

--- app/src/main/java/com/creativesourceapps/android/cupboard/CupboardFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/CupboardFragment.java
@@ -2,6 +2,7 @@ package com.creativesourceapps.android.cupboard;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,9 +18,11 @@ import java.util.ArrayList;
 public class CupboardFragment extends Fragment implements CupboardAdapter.ItemClickListener {
 
     private RecyclerView recyclerView;
+    private FloatingActionButton fab;
     private CupboardAdapter adapter;
     private ArrayList<String> categoryList;
     private GridLayoutManager layoutManager;
+    private DetailCupboardFragment fragment;
 
     public CupboardFragment() {
         // Required empty public constructor
@@ -31,14 +34,26 @@ public class CupboardFragment extends Fragment implements CupboardAdapter.ItemCl
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cupboard, container, false);
         recyclerView = view.findViewById(R.id.cupboard_grid_view);
+        fab = view.findViewById(R.id.fab_ingredient_list);
+        fragment = new DetailCupboardFragment();
         categoryList = new ArrayList<>();
         layoutManager = new GridLayoutManager(getContext(), 2);
         getCategories();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            fab.setTransitionName("All Ingredients");
             setExitTransition(TransitionInflater.from(getContext())
                     .inflateTransition(R.transition.grid_exit_transition));
         }
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startFragmentTransaction(-1, fab );
+
+            }
+        });
 
         return view;
     }
@@ -58,13 +73,14 @@ public class CupboardFragment extends Fragment implements CupboardAdapter.ItemCl
 
     }
 
-    @Override
-    public void onItemClicked(int clickedItem, LinearLayout itemLayout) {
-        DetailCupboardFragment fragment = new DetailCupboardFragment();
+    public void startFragmentTransaction(int clickedItem, View view) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Bundle bundle = new Bundle();
-            bundle.putString("Shared Element", categoryList.get(clickedItem));
+            if(clickedItem == -1)
+                bundle.putString("Shared Element", "All Ingredients");
+            else
+                bundle.putString("Shared Element", categoryList.get(clickedItem));
             fragment.setArguments(bundle);
             Transition transition = TransitionInflater.from(getContext())
                     .inflateTransition(R.transition.cupboard_shared_element_transition);
@@ -72,7 +88,7 @@ public class CupboardFragment extends Fragment implements CupboardAdapter.ItemCl
             fragment.setSharedElementEnterTransition(transition);
             getActivity().getSupportFragmentManager()
                     .beginTransaction()
-                    .addSharedElement(itemLayout, itemLayout.getTransitionName())
+                    .addSharedElement(view, view.getTransitionName())
                     .replace(R.id.fl_fragment, fragment)
                     .addToBackStack(null)
                     .commit();
@@ -84,5 +100,11 @@ public class CupboardFragment extends Fragment implements CupboardAdapter.ItemCl
                     .addToBackStack(null)
                     .commit();
         }
+    }
+
+    @Override
+    public void onItemClicked(int clickedItem, LinearLayout itemLayout) {
+
+        startFragmentTransaction(clickedItem, itemLayout);
     }
 }
