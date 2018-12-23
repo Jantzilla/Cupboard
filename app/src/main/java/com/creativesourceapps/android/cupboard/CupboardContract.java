7ab0555b8025d44f8befc2006e164e95658f1b30
Jantz <jantzillac.93@gmail.com>
2018-12-22 23:59:13 -0600

    Add column "category" to ingredient class database contract

--- app/src/main/java/com/creativesourceapps/android/cupboard/CupboardContract.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/CupboardContract.java
@@ -9,6 +9,7 @@ public class CupboardContract {
     public static class Ingredients implements BaseColumns {
         public static final String TABLE_NAME = "ingredients";
         public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_CATEGORY = "category";
         public static final String COLUMN_QUANTITY = "quantity";
         public static final String COLUMN_UNIT = "unit";
     }
