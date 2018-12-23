f17c1fe010ea076156ed3bab778ac591a4bf01e9
Jantz <jantzillac.93@gmail.com>
2018-12-23 01:44:21 -0600

    Add DbHelper class for creating & updating database

---
+++ app/src/main/java/com/creativesourceapps/android/cupboard/CupboardDbHelper.java
@@ -0,0 +1,36 @@
+package com.creativesourceapps.android.cupboard;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CupboardDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Cupboard.db";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + CupboardContract.Ingredients.TABLE_NAME + " (" +
+                    CupboardContract.Ingredients._ID + " INTEGER PRIMARY KEY," +
+                    CupboardContract.Ingredients.COLUMN_NAME + " TEXT," +
+                    CupboardContract.Ingredients.COLUMN_CATEGORY + " TEXT," +
+                    CupboardContract.Ingredients.COLUMN_QUANTITY + " INTEGER," +
+                    CupboardContract.Ingredients.COLUMN_UNIT + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + CupboardContract.Ingredients.TABLE_NAME;
+
+    public CupboardDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
