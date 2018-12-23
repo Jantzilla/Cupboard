ffb2e5075eff8f7cda78613ad4ed469d56435f2d
Jantz <jantzillac.93@gmail.com>
2018-12-23 11:54:35 -0600

    Add categorySpinner "listener" in add ingredient dialog
    This will listen for categorySpinner item selection & store the
    value of the selected position in the "category" variable.

--- app/src/main/java/com/creativesourceapps/android/cupboard/DetailCupboardFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/DetailCupboardFragment.java
@@ -118,6 +118,18 @@ public class DetailCupboardFragment extends Fragment {
                     }
                 });
 
+                categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
                 dialog.show();
             }
         });
