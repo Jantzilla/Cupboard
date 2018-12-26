4579df0e185e274bf9c15f30085e05386fa66051
Jantz <jantzillac.93@gmail.com>
2018-12-26 16:52:40 -0600

    Change Recipe adapter from ArrayAdapter to RecyclerView.Adapter

--- app/src/main/java/com/creativesourceapps/android/cupboard/RecipeListAdapter.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/RecipeListAdapter.java
@@ -1,39 +1,53 @@
 package com.creativesourceapps.android.cupboard;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class RecipeListAdapter extends ArrayAdapter<Recipe> {
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
 
-    /**
-     * Constructor method
-     * @param recipes The list of recipes to display
-     */
-    public RecipeListAdapter(Context context, ArrayList<Recipe> recipes) {
-        super(context,0,recipes);
+    private ArrayList<Recipe> recipes = new ArrayList<>();
+    private Context context;
+
+    public RecipeListAdapter(ArrayList<Recipe> recipes) {
+        this.recipes.addAll(recipes);
     }
 
-    /**
-     * Creates a new TextView for each item referenced by the adapter
-     */
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        Recipe recipes = getItem(position);
-        TextView textView;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_recipe_layout, parent, false);
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.grid_recipe_layout,viewGroup,false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder recipeViewHolder, int i) {
+        recipeViewHolder.titleTextView.setText(recipes.get(i).title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+        ImageView buttonImageView;
+
+        public RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.grid_item_recipe);
+            buttonImageView = itemView.findViewById(R.id.iv_button);
         }
-
-        textView = convertView.findViewById(R.id.grid_item_recipe);
-
-        // Set the text resource and return the newly created TextView
-        textView.setText(recipes.title);
-        return convertView;
     }
-
 }
