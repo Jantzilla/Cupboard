1201d0256a6810347064aea31ad5bf45ece184de
Jantz <jantzillac.93@gmail.com>
2018-12-27 04:10:27 -0600

    Remove "toolbar" with "floatingSearchView" in MainActivity

--- app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/MainActivity.java
@@ -6,17 +6,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private FloatingSearchView floatingSearchView;
     private DrawerLayout drawerLayout;
     private FragmentManager fragmentManager;
     private Fragment fragment;
@@ -43,15 +43,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
+        floatingSearchView = findViewById(R.id.floating_search_view);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation);
         fragmentManager = getSupportFragmentManager();
         fragment = new RecipeFragment();
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        floatingSearchView.attachNavigationDrawerToMenuButton(drawerLayout);
 
         fragmentManager.beginTransaction().add(R.id.fl_fragment, fragment).commit();
 
