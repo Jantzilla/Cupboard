2789a32aa0901439885fc2ec0b02ed1dc0205f48
Jantz <jantzillac.93@gmail.com>
2019-01-01 23:07:26 -0600

    Add recipe ingredients "Grid-RecyclerView" implementation

--- app/src/main/java/com/creativesourceapps/android/cupboard/IngredientFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/IngredientFragment.java
@@ -2,11 +2,17 @@ package com.creativesourceapps.android.cupboard;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class IngredientFragment extends Fragment {
+    RecyclerView recyclerView;
+    private Recipe recipe;
+    private IngredientListAdapter adapter;
+    private GridLayoutManager gridLayoutManager;
 
     public IngredientFragment() {
         // Required empty public constructor
@@ -17,6 +23,15 @@ public class IngredientFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ingredient, container, false);
+
+        recyclerView = view.findViewById(R.id.rv_ingredients);
+        recipe = getActivity().getIntent().getParcelableExtra("parcel_data");
+        adapter = new IngredientListAdapter(getContext(), "Ingredients", recipe.quantity, recipe.unit, recipe.ingredients);
+
+        gridLayoutManager = new GridLayoutManager(getContext(), 3);
+
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
