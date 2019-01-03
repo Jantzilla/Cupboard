13657dc038d263ad489d14452ce9bac992719c66
Jantz <jantzillac.93@gmail.com>
2019-01-02 21:09:30 -0600

    Change Recipe object's "media" variable from ArrayList to String

--- app/src/main/java/com/creativesourceapps/android/cupboard/Recipe.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/Recipe.java
@@ -7,16 +7,15 @@ import java.util.ArrayList;
 
 public class Recipe implements Parcelable {
     final int id;
-    final String title;
+    final String title, media;
     ArrayList<String> unit;
     ArrayList<String> quantity;
     ArrayList<String> ingredients;
     ArrayList<String> steps;
     ArrayList<String> instructions;
-    ArrayList<String> media;
 
     public Recipe(int id, String title, ArrayList<String> ingredients, ArrayList<String> quantity, ArrayList<String> unit,
-                  ArrayList<String> shortDescription, ArrayList<String> description, ArrayList<String> media)
+                  ArrayList<String> shortDescription, ArrayList<String> description, String media)
     {
         this.id = id;
         this.title = title;
@@ -31,18 +30,17 @@ public class Recipe implements Parcelable {
     private Recipe(Parcel in) {
         id = in.readInt();
         title = in.readString();
+        media = in.readString();
         ingredients = new ArrayList<String>();
         quantity = new ArrayList<String>();
         unit = new ArrayList<String>();
         steps = new ArrayList<String>();
         instructions = new ArrayList<String>();
-        media = new ArrayList<String>();
         in.readList(ingredients, getClass().getClassLoader());
         in.readList(quantity, getClass().getClassLoader());
         in.readList(unit, getClass().getClassLoader());
         in.readList(steps, getClass().getClassLoader());
         in.readList(instructions, getClass().getClassLoader());
-        in.readList(media, getClass().getClassLoader());
     }
 
     public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
@@ -66,11 +64,11 @@ public class Recipe implements Parcelable {
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeInt(id);
         parcel.writeString(title);
+        parcel.writeString(media);
         parcel.writeList(ingredients);
         parcel.writeList(quantity);
         parcel.writeList(unit);
         parcel.writeList(steps);
         parcel.writeList(instructions);
-        parcel.writeList(media);
     }
 }
