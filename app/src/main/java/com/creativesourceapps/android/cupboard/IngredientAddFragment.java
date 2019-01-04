9524f4ddf71701fbc6445395b85ca6f0006d79d5
Jantz <jantzillac.93@gmail.com>
2019-01-04 01:51:51 -0600

    Add "FAB" to exit from the "IngredientAddFragment"

--- app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
@@ -16,7 +16,16 @@ public class IngredientAddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredient_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_ingredient_add, container, false);
+
+        ((MainActivity)getActivity()).setScrimVisibility(true);
+
+        return view;
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        ((MainActivity)getActivity()).setScrimVisibility(false);
+    }
 }
