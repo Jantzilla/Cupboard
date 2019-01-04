cb5ca8f022a9443392576bde87fee6ceacd1da1c
Jantz <jantzillac.93@gmail.com>
2019-01-04 02:19:53 -0600

    Add "click listener" to FAB button to exit "Add Fragment"

--- app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
@@ -1,12 +1,14 @@
 package com.creativesourceapps.android.cupboard;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class IngredientAddFragment extends Fragment {
+    FloatingActionButton fab;
 
     public IngredientAddFragment() {
         // Required empty public constructor
@@ -17,6 +19,15 @@ public class IngredientAddFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ingredient_add, container, false);
+
+        fab = view.findViewById(R.id.fab_back);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
 
         ((MainActivity)getActivity()).setScrimVisibility(true);
 
