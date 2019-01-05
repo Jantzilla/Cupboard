40de6488d3cd3cfe3717e8c0025bc29f700a2a3f
Jantz <jantzillac.93@gmail.com>
2019-01-05 00:08:01 -0600

    Move "Add Ingredient" SQLite statements to IngredientAddFragment

--- app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/IngredientAddFragment.java
@@ -1,14 +1,27 @@
 package com.creativesourceapps.android.cupboard;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
 
 public class IngredientAddFragment extends Fragment {
-    FloatingActionButton fab;
+    FloatingActionButton fab, addFab;
+    private SQLiteDatabase db;
+    private CupboardDbHelper dbHelper;
+    private Cursor cursor;
+    private EditText ingredientEditText, quantityEditText;
+    private String selectedUnit;
+    private boolean savedIngredient;
+    private String selectedCategory;
+    private String[] projection;
 
     public IngredientAddFragment() {
         // Required empty public constructor
@@ -30,6 +43,63 @@ public class IngredientAddFragment extends Fragment {
         });
 
         ((MainActivity)getActivity()).setScrimVisibility(true);
+
+        ingredientEditText = view.findViewById(R.id.tv_ingredient);
+        quantityEditText = view.findViewById(R.id.tv_quantity);
+        dbHelper = new CupboardDbHelper(getContext());
+
+        selectedUnit = "g";              //Reset default ingredient selectedUnit
+
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ingredientEditText.getText().toString().isEmpty())
+                    ingredientEditText.setError("Please enter a name.");
+                if(quantityEditText.getText().toString().isEmpty())
+                    quantityEditText.setError("Please enter a quantity.");
+                else {
+
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.name = ingredientEditText.getText().toString();
+                    ingredient.quantity = Integer.valueOf(quantityEditText.getText().toString());
+                    ingredient.unit = selectedUnit;
+                    ingredient.category = selectedCategory;
+
+                    db = dbHelper.getWritableDatabase();
+
+                    cursor = db.query(
+                            CupboardContract.Ingredients.TABLE_NAME,
+                            projection,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                    );
+
+                    while(cursor.moveToNext()) {
+                        if(cursor.getString(cursor.getColumnIndex(CupboardContract.Ingredients.COLUMN_NAME))
+                                .equals(ingredient.name)) {
+                            ingredientEditText.setError("Ingredient already exists.");
+                            savedIngredient = true;
+                        }
+                    }
+
+                    if(!savedIngredient) {
+                        ContentValues values = new ContentValues();
+                        values.put(CupboardContract.Ingredients.COLUMN_NAME, ingredient.name);
+                        values.put(CupboardContract.Ingredients.COLUMN_QUANTITY, ingredient.quantity);
+                        values.put(CupboardContract.Ingredients.COLUMN_UNIT, ingredient.unit);
+                        values.put(CupboardContract.Ingredients.COLUMN_CATEGORY, ingredient.category);
+                        db.insert(CupboardContract.Ingredients.TABLE_NAME, null, values);
+
+                    }
+
+                    savedIngredient = false;
+
+                }
+            }
+        });
 
         return view;
     }
