fe73267fad4d10603f2070a7711ba9c433d03777
Jantz <jantzillac.93@gmail.com>
2019-01-27 23:44:36 -0600

    Make changes to "RecipeUtils.parseMeasure()" parse algorithm

--- app/src/main/java/com/creativesourceapps/android/cupboard/RecipeUtils.java
+++ app/src/main/java/com/creativesourceapps/android/cupboard/RecipeUtils.java
@@ -31,19 +31,20 @@ public class RecipeUtils {
                 .replaceAll("1\\/3|\u2153|3rd", ".33").replaceAll("3\\/4|\u00BE", ".75")
                 .replaceAll("1\\/8|\u215B", ".13").replaceAll("-", "");
 
-        result = result.replaceAll("\\/\\d+\\D+", "");
+        result = result.replaceAll("\\/\\d+\\D+.*", "");
 
         //Consistent Unit
-        result = result.replaceAll("TSP|Tsp|Teaspoon|teaspoon", "tsp");
+        result = result.replaceAll("TSP|Tsp|Teaspoons|teaspoons|Teaspoon|teaspoon", "tsp");
         result = result.replaceAll("TBS|Tbs|Tbsp|tbs\\w*|TBLSP|Tblsp|tblsp|Tbls|tbls|Tablespoons|tablespoons|Tablespoon|tablespoon", "tbsp");
         result = result.replaceAll("lb|LB|LBS|Pounds|Pound|pound|pounds", "lbs");
         result = result.replaceAll("OZ|Ounce|Ounces|OUNCES|OUNCE|ounce", "oz");
         result = result.replaceAll("mL|ML|Ml", "ml");
+        result = result.replaceAll("L|Liters|liters|Liter|liter", "l");
         result = result.replaceAll("grams|Grams|G", "g");
         result = result.replaceAll("KGS|Kgs|kgs|KG|Kg|kg|kilograms|Kilograms", "kg");
         result = result.replaceAll("QTS|Qts|qts|QT|Qt|qt|Quarts|quarts|Quart|quart", "qt");
         result = result.replaceAll("CUPS|Cups|CUP|Cup|cups|cup", "cup");
-        result = result.replaceAll("Dash|Pinch|Sprinkling|Topping|drizzle|garnish|dusting|Dusting", "1 dash"); // (subtract "1 tsp")
+        result = result.replaceAll("Dash|Pinches|pinches|Pinch|pinch|Sprinkling|sprinkling|sprinking|spinkling|Topping|drizzle|garnish|dusting|Dusting", "1 dash"); // (subtract "1 tsp")
         result = result.replaceAll("to serve", "1 pcs");
 
         result = result.trim();
@@ -54,11 +55,14 @@ public class RecipeUtils {
         // - Remove string after "/"
         // - Remove string between "()" inclusive
         // - Replace string containing isolated " x " with "1 pcs"
+        // - Lastly Remove first number if there are two ex.(3 400 g)
+        // - Fix error 650g/1lb 8oz equals 650  oz
 
         //Add Spacing
         result = Pattern.compile("\\d*\\.*\\d+lbs").matcher(result).find() ? result.replaceAll("lbs", " lbs") : result;
         result = Pattern.compile("\\d*\\.*\\d+g").matcher(result).find() ? result.replaceAll("g", " g") : result;
         result = Pattern.compile("\\d*\\.*\\d+ml").matcher(result).find() ? result.replaceAll("ml", " ml") : result;
+        result = Pattern.compile("\\d*\\.*\\d+l").matcher(result).find() ? result.replaceAll("l", " l") : result;
         result = Pattern.compile("\\d*\\.*\\d+oz").matcher(result).find() ? result.replaceAll("oz", " oz") : result;
         result = Pattern.compile("\\d*\\.*\\d+tsp").matcher(result).find() ? result.replaceAll("tsp", " tsp") : result;
         result = Pattern.compile("\\d*\\.*\\d+tbsp").matcher(result).find() ? result.replaceAll("tbsp", " tbsp") : result;
@@ -67,7 +71,11 @@ public class RecipeUtils {
         result = Pattern.compile("\\d*\\.*\\d+cup").matcher(result).find() ? result.replaceAll("cup", " cup") : result;
 
         //Match words([Aa-Zz]) not equal to above unit keywords & remove
-        result = result.replaceAll("\\b(?!\\d|(\\.)|pcs|tsp|tbsp|lbs|oz|qt|dash|g\\b|kg|ml|cup\\b)\\w+","");
+        result = result.replaceAll("\\b(?!\\d+\\b|(\\.)|pcs|tsp|tbsp|lbs|oz|qt|dash|g\\b|kg|ml|l\\b|cup\\b)\\w+","");
+
+        if(result.split("\\s+").length > 2) {
+            result = result.replaceAll("^\\d+\\w*", "");
+        }
 
         result = result.trim();
 
@@ -77,7 +85,7 @@ public class RecipeUtils {
         if(result.matches("\\d+|\\d*\\.\\d+"))
             result += " pcs";
 
-//        Log.d("Debug", result + " - ( " + s + " ) " ); TODO: REMOVE AFTER TESTING IS COMPLETE
+        Log.d("Debug", result + " - ( " + s + " ) " );
 
         if(result.matches("\\D+")) {
             String tempResult = "1 " + result;
