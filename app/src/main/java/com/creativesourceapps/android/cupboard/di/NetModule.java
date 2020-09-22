20d13082bba6bca7bc8c85327e6867a11e9314b7
Jantz <jantzillac.93@gmail.com>
2020-09-22 06:30:01 -0500

    Create Dagger modules "AppModule" & "NetModule"

---
+++ app/src/main/java/com/creativesourceapps/android/cupboard/di/NetModule.java
@@ -0,0 +1,37 @@
+package com.creativesourceapps.android.cupboard.di;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+
+@Module
+public class NetModule {
+
+//    String mBaseUrl;
+//
+//    public NetModule(String baseUrl) {
+//        this.mBaseUrl = baseUrl;
+//    }
+
+    @Provides
+    @Singleton
+    Cache provideOkHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024; // 10 MiB
+        Cache cache = new Cache(application.getCacheDir(), cacheSize);
+        return cache;
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache) {
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.cache(cache);
+        return client.build();
+    }
+
+}
