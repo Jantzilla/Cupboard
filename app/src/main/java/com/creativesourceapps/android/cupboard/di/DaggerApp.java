bcd7b537e6cd9854ce3871473380da406e37498e
Jantz <jantzillac.93@gmail.com>
2020-09-22 06:32:16 -0500

    Create custom application class "DaggerApp"

---
+++ app/src/main/java/com/creativesourceapps/android/cupboard/di/DaggerApp.java
@@ -0,0 +1,21 @@
+package com.creativesourceapps.android.cupboard.di;
+
+import android.app.Application;
+
+public class DaggerApp extends Application {
+
+    private AppComponent mAppComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mAppComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return mAppComponent;
+    }
+}
